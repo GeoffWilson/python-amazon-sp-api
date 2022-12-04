1710526e150a983912c96c93643ad477db8a79af
Geoff Wilson <gw@netxl.com>
2022-12-04 21:00:32 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='amazon-sp-api',
+    version='0.1',
+    packages=['amazon_sp_api'],
+    url='https://www.netxl.com',
+    license='',
+    author='NetXL Distrubtion Ltd',
+    author_email='help@netxl.com',
+    description='',
+    requires=['requests', 'boto3']
+)
