21fd2ca1456af33ab9c76e4a10870eda4c6bbe00
Geoff Wilson <gw@netxl.com>
2025-01-28 11:10:25 +0000

    Remove logging line.

--- amazon_sp_api/client.py
+++ amazon_sp_api/client.py
@@ -114,8 +114,6 @@ class Client(object):
             query_string=request.query_string
         )
 
-        print(outcome.content)
-
         if not outcome.ok:
             raise ValueError('Amazon SP-API call failed')
 
